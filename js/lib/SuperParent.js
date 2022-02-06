af2deb375e1d7b34f24398e70735f0a9a575f86e
sakthiprasath <sakthiprasath25@gmail.com>
2022-02-06 12:14:04 +0530

    superparent js modified to cache all tree children

--- js/lib/SuperParent.js
+++ js/lib/SuperParent.js
@@ -6,58 +6,64 @@ export default class SuperParent {
     this.action_map = {};
     this.cache_elems = {};
   }
-
   before_render() {
     return $.Deferred().resolve();
   }
-
   events() {
     let self = this;
     /* initializing eventlistners by calling above event_map in a loop*/
     for (let key in self.event_map) {
-      //          console.log(self.event_map[key]);
       self.event_map[key]();
     }
   }
-
-  init_nested_ele(selector, class_name) {
+  init_nested_ele(selector, class_name, props) {
     let self = this;
-    self.tsp = new class_name(self.tsp, selector).init();
+    self.tsp = new class_name(self.tsp, selector).init(props);
+  }
+
+  cache_tree(root) {
+    /*Recusive function that takes tree root as argument and stores the   below elems in this.cache_elems
+      1. root
+      2. All the Child elemtes
+      3. All the Child of Child elements
+    */
+    let self = this;
+    let selector_name = root.attr('selector');
+    if (selector_name !== undefined) 
+    self.cache_elems[selector_name] = root;
+    let children = root.children();
+    for (let i = 0; i < children.length; i++) {
+      let child_ele = $(children[i]);
+      self.cache_tree(child_ele);
+    }
   }
 
   HTMLGenerator(html_str) {
     let self = this;
-    let generate_ele = $(html_str);
-    let children = generate_ele.children();
-    for (let i = 0; i < children.length; i++) {
-      let child_ele = $(children[i]);
-      let selector = child_ele.attr('selector');
-      console.log(selector);
-      self.cache_elems[selector] = child_ele;
-      // self.proper (selector, selector);
-      // console.log(children[child]);
-    }
-    $(self.parent_element).append(generate_ele);
+    let root_ele = $(html_str);
+
+    self.cache_tree(root_ele);
+
+    $(self.parent_element).append(root_ele);
   }
 
   after_render() {}
-  
   include_css(css_content) {
     let style_tag = $('<style></style>');
     style_tag.append(css_content);
-    console.log(css_content);
     $(this.parent_element).append(style_tag);
   }
 
   include_css_from_url(url) {
-    $.get(url).then((css) => {
-      console.log(css);
-    });
+    // $.get(url).then( (css)=>{
+    //   console.log(css);
+    // });
   }
 
-  init() {
+  init(props) {
     let def = $.Deferred();
     let self = this;
+    self.props = props;
     self.set_action_map();
     self.before_render().then(function () {
       let html_str = self.render();
